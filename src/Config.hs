5b3c59eb102db68262de15e2ed423d725ad66cd0
smelc <clement.hurlin@gmail.com>
2019-11-05 14:37:52 +0100

    Implement $[...] substitutions

--- src/Config.hs
+++ src/Config.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Config where
 
+import Data.Either
 import Data.List
 import Text.Printf
 import qualified Data.Map.Strict as Map
@@ -16,7 +17,8 @@ data Config = Config { builders :: [Builder], subst :: Subst }
 type Subst = Map.Map String String
 
 class Substable a where
-    substitute :: Subst -> a -> a
+    -- The result of applying a substitution (Right) or an error message (Left)
+    substitute :: Subst -> a -> Either String a
 
 -- split_around 'b' 'foobar' = Just(("foo", "ar"))
 -- splitAround 'f' 'foobar' = Just(("", "oobar"))
@@ -38,7 +40,7 @@ splitAround sep text =
 -- splitDelimiters "(" ")" "(var)bar" returns Just("", "var", "bar")
 -- splitDelimiters "(" ")" "foo(var)" returns Just("foo", "var")
 -- splitDelimiters "(" ")" "foobar" returns Nothing
-splitDelimiters :: String -> String -> String -> Maybe(String, String, String)
+splitDelimiters :: String -> String -> String -> Maybe (String, String, String)
 -- This implementation is not very haskellish. I suppose we could use some Functor
 -- to simplify Nothing handling
 splitDelimiters open close text = do
@@ -46,19 +48,27 @@ splitDelimiters open close text = do
   (beforeClose, afterClose) <- splitAround close afterOpen
   return (beforeOpen, beforeClose, afterClose)
 
-substituteString :: Subst -> String -> String
-substituteString subst s = s -- FIXME
+-- Replace $[key] by subst[key] for every $[key] found
+-- Fail (return Left) if some key is not mapped by the substitution
+applySubstitution :: Subst -> String -> Either String String
+applySubstitution subst text =
+    case splitDelimiters "$[" "]" text of
+        Nothing -> Right text
+        Just (before, key, after) ->
+            case Map.lookup key subst of
+                Nothing -> Left ("$[" ++ key ++ "] appears in " ++ text ++ " but " ++ key ++ " is not bound by the substitution (" ++ show subst ++ ")")
+                Just image -> applySubstitution subst (before ++ image ++ after)
 
 attrValueString :: String -> String -> String
 attrValueString attr value = printf "%s=\"%s\"" attr value
 
+----------------------------
+-- Implementation of Show --
+----------------------------
+
 instance Show Step where
     show (SetPropertyFromValue prop value)= printf "<set_property %s/>" (attrValueString prop value)
     show (ShellCmd cmdList)= "<shell command=\"" ++ (unwords cmdList) ++ "\"/>"
-
-instance Substable Step where
-    substitute subst (SetPropertyFromValue prop value) = SetPropertyFromValue prop (substituteString subst value)
-    substitute subst whatever = whatever
 
 instance Show Builder where
     show (Builder name steps) = 
@@ -66,3 +76,39 @@ instance Show Builder where
             indentedSteps :: [String] = map (\x -> " " ++ x) shownSteps
             linedSteps :: String = intercalate "\n" indentedSteps
          in "<builder>\n" ++ linedSteps ++ "\n</builder>"
+
+---------------------------------
+-- Implementation of Substable --
+---------------------------------
+
+-- Lift substitute to lists
+instance Substable a => Substable [a] where
+    substitute subst list = 
+        let base :: [Either String a] = map (substitute subst) list
+            (failures, images) = partitionEithers base in
+                if length(failures) > 0
+                then Left(intercalate "\n" failures) -- report all errors at once, not only the first one
+                else Right images
+
+instance Substable Step where
+    substitute subst (SetPropertyFromValue prop value) = do
+        valueImage <- applySubstitution subst value
+        Right (SetPropertyFromValue prop valueImage)
+    substitute subst (ShellCmd cmdList) =
+        let cmdListImage :: [Either String String] = map (applySubstitution subst) cmdList
+            (failures, images) = partitionEithers cmdListImage in
+                if length(failures) > 0
+                then Left(intercalate "\n" failures)
+                else Right (ShellCmd images)
+
+instance Substable Builder where
+    -- can this be written in a more haskellish way ?
+    substitute subst (Builder name steps) = 
+        case substedName of
+            Left errMsg -> Left errMsg
+            Right nameImage ->
+                case substedSteps of
+                    Left errMsg -> Left errMsg
+                    Right stepsImage -> Right $ Builder nameImage stepsImage
+        where substedName :: Either String String = applySubstitution subst name
+              substedSteps :: Either String [Step] = substitute subst steps
--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,7 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
+
+import qualified Data.Map.Strict as Map
 
 import Config
 
@@ -6,7 +9,9 @@ main :: IO ()
 main = do
   putStrLn $ show shellCmd0
   putStrLn $ show builder
-  where shellCmd0 :: Step
-        shellCmd0 = ShellCmd ["ls", "/media"]
-        shellCmd1 = ShellCmd ["cd", "/media"]
-        builder = Builder "bname" [shellCmd0, shellCmd1]
+  let substedBuilder :: Either String Builder = substitute subst builder
+  putStrLn $ show substedBuilder
+  where shellCmd0 :: Step = ShellCmd ["ls", "$[mydir]"]
+        shellCmd1 = ShellCmd ["cd", "$[mydir]"]
+        builder :: Builder = Builder "bname" [shellCmd0, shellCmd1]
+        subst :: Subst = Map.fromList [("mydir", "/home/churlin")]
