7b4b400eccbb4f2d2d39415fa44d7f7dc044cf5d
smelc <clement.hurlin@gmail.com>
2019-11-13 07:27:24 +0100

    Fix CI build (hlint)

--- src/Exec.hs
+++ src/Exec.hs
@@ -17,7 +17,7 @@ createStepProcess cmd = createProcess (shell $ unwords cmd){ std_out = CreatePip
 
 -- return code, stdout, stderr
 runShellCommand :: [String] -> IO (ExitCode, String, String)
-runShellCommand cmd = readProcessWithExitCode ("/bin/sh") cmd ""
+runShellCommand cmd = readProcessWithExitCode "/bin/sh" cmd ""
 
 runStep :: BuildContext -> Step -> Either BuildContext (IO (ExitCode, String, String)) 
 runStep ctxt step = 
@@ -25,7 +25,7 @@ runStep ctxt step =
         SetPropertyFromValue prop value -> Left $ Map.insert prop value ctxt
         ShellCmd cmd -> Right $ runShellCommand cmd
 
-runSteps :: BuildContext -> [Step] -> IO(ExitCode)
+runSteps :: BuildContext -> [Step] -> IO ExitCode
 runSteps ctxt [] = return ExitSuccess
 runSteps ctxt (step : tl) =
     case runStep ctxt step of
@@ -34,7 +34,7 @@ runSteps ctxt (step : tl) =
             (rc, stdin, stdout) <- io
             case rc of
                 ExitSuccess -> runSteps ctxt tl -- step succeeded, continue execution
-                otherwise -> return rc           -- step failed; stop execution
+                _           -> return rc        -- step failed; stop execution
 
-runBuild :: Builder -> IO(ExitCode)
+runBuild :: Builder -> IO ExitCode
 runBuild (Builder _ steps) = runSteps Map.empty steps
