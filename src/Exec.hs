f4dd27eeb268c1f1a64326c783efa74c9a10cb78
Paul Brauner <polux@google.com>
2019-11-14 12:46:27 +0100

    Use prettyprinter-ansi-terminal for colors. It is more declarative because it separates the coloring from the printing.

--- src/Exec.hs
+++ src/Exec.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- File to exec a build specified as a Builder
 module Exec where
 
 import Control.Monad
-import System.Console.ANSI
+import Data.Text.Prettyprint.Doc
+import Data.Text.Prettyprint.Doc.Render.Terminal
 import System.Exit
 import System.IO
 import System.Process
@@ -17,15 +19,16 @@ import qualified GHC.IO.Handle as Handle
 -- Maps build variables to their values
 type BuildContext = Map.Map String String
 
-zzLog :: String -> IO()
-zzLog log = putStrLn $ "ZZ> " ++ log
+zzLog :: Color -> String -> IO()
+zzLog textColor logEntry = putDoc (annotate style doc)
+ where
+  doc = "ZZ>" <+> pretty logEntry <> hardline
+  style = color textColor
 
 -- return code, stdout, stderr
 runShellCommand :: [String] -> IO (ExitCode, String, String)
 runShellCommand cmd = do
-    setSGR [SetColor Foreground Vivid Green]
-    zzLog $ unwords cmd
-    setSGR []
+    zzLog Green (unwords cmd)
     readProcessWithExitCode "/bin/sh" ("-c" : cmd) ""
 
 runSteps :: BuildContext -> [Step] -> IO ExitCode
@@ -33,15 +36,13 @@ runSteps ctxt [] = return ExitSuccess
 runSteps ctxt (SetPropertyFromValue prop value : steps) =
   runSteps (Map.insert prop value ctxt) steps
 runSteps ctxt (ShellCmd cmd : steps) = do
-  (rc, outmsg :: String, errmsg) <- runShellCommand cmd
+  (rc, outmsg, errmsg) <- runShellCommand cmd
   unless (null outmsg) $ putStr outmsg -- show step normal output, if any
   unless (null errmsg) $ hPutStrLn stderr errmsg -- show step error output, if any
   case rc of
     ExitSuccess -> runSteps ctxt steps -- step succeeded, continue execution
     _           -> do                  -- step failed, stop execution
-      setSGR [SetColor Foreground Vivid Red]
-      zzLog $ unwords cmd ++ " failed: " ++ show rc
-      setSGR []
+      zzLog Red (unwords cmd ++ " failed: " ++ show rc)
       return rc
 
 runBuild :: Builder -> IO ExitCode
