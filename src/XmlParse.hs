a25eef4b80931f31b08ee3cd51138a868d3fc839
smelc <clement.hurlin@gmail.com>
2019-11-20 17:36:11 +0100

    Build own AST out of XML's values + start <builder> parsing

--- src/XmlParse.hs
+++ src/XmlParse.hs
@@ -21,88 +21,43 @@ giveLineInMsg :: String -> Maybe Line -> String
 giveLineInMsg errMsg Nothing = errMsg
 giveLineInMsg errMsg (Just line) = "At line " ++ show line ++ ": " ++ errMsg
 
--- Check that Content is an Element with the given name
-checkElementAndName :: String -> Content -> Either String Element
-checkElementAndName expected (Elem res@Element {elName=elName, elLine=elLine}) =
-    case elName of
-        QName {qName=qName} ->
-            if qName == expected then Right res
-            else Left $ giveLineInMsg errMsg elLine
-            where errMsg = "Element has name " ++ qName ++ " while " ++ expected ++ " was expected"
-checkElementAndName expected _ = Left $ "Element named " ++ expected ++ " not found"
+data ZXML = ZElem {tag :: String, attrs :: [(String, String)], maybeLine :: Maybe Line}
 
-type ZZAttr = (String, String) -- ^ An attribute name, and its value
+getAttrValue :: String -- ^ The element in which the attribute is being searched (for error messages)
+             -> String -- ^ The attribute's name
+             -> [(String, String)] -- ^ The actual list of attributes, with the value
+             -> Either String String -- ^ An error message or the attribute's value
+getAttrValue elem attr attrs =
+    case value of
+        Nothing    -> Left ("Missing attribute in element " ++ elem ++ ": " ++ attr)
+        Just value -> Right value
+    where value = fmap fst (find (\entry -> fst entry == attr) attrs)
 
--- | Translate xml's Attr to ZZAttr. The translation ignores qURI and qPrefix
-zzAttrs :: [Attr] -> [ZZAttr]
-zzAttrs = map zzAttr
-    where zzAttr Attr {attrKey=attrKey, attrVal=val} = (extractName attrKey, val)
-          extractName QName {qName=qName} = qName
-
-parseAttribute :: String     -- ^ The element being inspected
-               -> String     -- ^ A required attribute
-               -> [ZZAttr]   -- ^ The actual attributes
-               -> Maybe Line -- ^ The line of the element being parsed
-               -> Either String (String, String) -- ^ Either an error message
-                                                 -- or the attribute and its value
-parseAttribute element attr actuals maybeLine = 
-    case find (\pair -> fst pair == attr) actuals of
-        Nothing -> Left $ giveLineInMsg ("Attribute " ++ attr ++ " not found. Arguments found: " ++ show(map fst actuals)) maybeLine
-        Just (_, v) -> Right (attr, v)
-
-
-parseAttributes :: String     -- ^ The element being inspected
-                -> [String]   -- ^ The required attributes
-                -> [ZZAttr]   -- ^ The actual attributes
-                -> Maybe Line -- ^ The line of the element being parsed
-                -> Either String (Map.Map String String) -- ^ Either an error message
-                -- or a mapping from the required attributes to their values
-parseAttributes element required actuals maybeLine = 
-    if not $ null errors
-    then Left $ unlines errors
-    else Right $ Map.fromList mappings
-    where eithers :: [Either String (String, String)]
-          eithers = map (\attr -> parseAttribute element attr actuals maybeLine) required
-          (errors, mappings) = partitionEithers eithers
-
-parseSetProperty :: [ZZAttr] -> Maybe Line -> Either String Step
-parseSetProperty actuals mLine = do
-    attrs <- parseAttributes "setProperty" [propertyAttr, valueAttr] actuals mLine
-    return SetPropertyFromValue{prop = fromJust $ Map.lookup propertyAttr attrs, 
-                                value = fromJust $ Map.lookup valueAttr attrs}
-    where propertyAttr = "property"
-          valueAttr = "value"
-
-parseShell :: [ZZAttr] -> Maybe Line -> Either String Step
-parseShell actuals mLine = do
-    attrs <- parseAttributes "shell" [commandAttr] actuals mLine
-    return ShellCmd{cmd = words (fromJust $ Map.lookup commandAttr attrs)} 
-    where commandAttr = "command"
-
-parseStep :: Content -> Either String Step
-parseStep (Elem Element {elName=elName, elAttribs=elAttribs, elLine=elLine}) =
-    case elName of
-        QName {qName=qName}
-            | qName == "setProperty" -> parseSetProperty (zzAttrs elAttribs) elLine
-            | qName == "shell"       -> parseShell (zzAttrs elAttribs) elLine
-            | otherwise              -> Left $ giveLineInMsg ("Unexpected element: " ++ qName ++ ". Expected one of: 'setProperty', 'shell'") elLine
-parseStep content = Left $ "Cannot parse Step from " ++ show content
-
--- Parses a builder from a Content
-parseBuilderContent :: Content -> Either String Builder
-parseBuilderContent content = do
-    builder <- checkElementAndName "builder" content
+zXMLToBuilder :: ZXML -> Either String Builder
+zXMLToBuilder zxml = do
+    name <- getAttrValue elem "name" zattrs
     undefined
+    where elem = "builder"
+          zattrs = attrs zxml
 
--- Parses builders from a String
-parseXmlString :: String -> IO Builder
+textXMLToZXML :: Content -> Either String ZXML
+textXMLToZXML (Text CData {cdLine=cdLine}) = Left $ giveLineInMsg "Unexpected Text in XML" cdLine
+textXMLToZXML (CRef _)                     = Left "Unexpected CRef in XML"
+textXMLToZXML (Elem Element {elName=elName, elAttribs=elAttribs, elLine=elLine}) =
+    return $ ZElem tag attrs elLine
+    where tag :: String = qName elName
+          attrs = map (\elAttr -> (qName $ attrKey elAttr, attrVal elAttr)) elAttribs
+
+parseXmlString :: String                  -- ^ The XML Content
+               -> Either String [Builder] -- ^ Either an error message, or the builders decoded from XML
 parseXmlString xml =
     undefined
     where contents :: [Content] = XmlInput.parseXML xml
+          zxmls = map textXMLToZXML contents
 
-parseXmlFile :: String     -- ^ A filename
-             -> IO Builder -- ^ The builder
+parseXmlFile :: String                       -- ^ A filename
+             -> IO (Either String [Builder]) -- ^ The builders
 parseXmlFile filepath = do
     handle :: Handle <- openFile filepath ReadMode  
     contents :: String <- hGetContents handle 
-    parseXmlString contents
+    return $ parseXmlString contents
