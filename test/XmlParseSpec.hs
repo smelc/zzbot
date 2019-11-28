cc053827192d8224137dc631b93d2bff95954e77
Paul Brauner <polux@google.com>
2019-11-28 10:05:53 +0100

    Better error messages. Don't continue collecting errors assuming a tag was correct if the tag isn't correct. Also return meaningful errors if the tag doesn't match any of the expected tags.

--- test/XmlParseSpec.hs
+++ test/XmlParseSpec.hs
@@ -1,24 +1,33 @@
 module XmlParseSpec (spec) where
 
 import Config
+import Control.Lens.Extras
 import Data.Validation
 import XmlParse
 import System.Exit
 import Test.Hspec
 
-isFailure :: Validation err a -> Bool
-isFailure (Failure _)  = True
-isFailure (Success _)  = False
-
-isSuccess :: Validation err a -> Bool
-isSuccess = not . isFailure
-
 badXml1 = "<foobar></foobar>"
+
+validXml =
+  "<builder name=\"ls builder\">\
+  \  <shell command=\"ls /\"/>\
+  \  <setProperty property=\"prop\" value=\"foobar\"/>\
+  \</builder>"
+
+expectedBuilder =
+  Builder
+    { name = "ls builder"
+    , steps =
+      [ ShellCmd { cmd = ["ls", "/"] }
+      , SetPropertyFromValue { prop = "prop", value = "foobar" }
+      ]
+    }
 
 spec :: SpecWith ()
 spec =
-  describe "testBadTopLevelElement" $
-    it "" $ do
-        print failure1
-        isFailure failure1 `shouldBe` True
-  where failure1 = parseXmlString badXml1
+  describe "parseXmlString" $ do
+    it "should succeed on valid XLM" $
+      parseXmlString validXml `shouldBe` Success [expectedBuilder]
+    it "should fail on bad toplevel element" $
+      parseXmlString badXml1 `shouldSatisfy` is _Failure
