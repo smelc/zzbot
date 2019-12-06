659938e1fcb8eb338a062f18c9650ad7832dbdfe
smelc <clement.hurlin@gmail.com>
2019-12-06 14:56:31 +0100

    Add option --print. There are extra new lines printed which needs to be sorted out to make it look tight.

--- src/Main.hs
+++ src/Main.hs
@@ -18,15 +18,22 @@ import qualified Data.Map.Strict as Map
 import qualified Data.Set as Set
 import qualified Options.Applicative as Opt
 
-process :: String -> IO ExitCode
-process filepath = do
+process :: Bool -- ^ Whether to print (True) or execute the builder (False)
+        -> String -- ^ The file containing the builder's description
+        -> IO ExitCode
+process printOrExec filepath  = do
   mbuilders :: XmlValidation (NonEmpty Builder) <- parseXmlFile filepath
   case mbuilders of
       Failure (err :: Set.Set XmlParsingError) -> do
         putStrLn $ unlines $ map show $ Set.toList err
         return (ExitFailure 1)
       Success (builders :: NonEmpty Builder) ->
-        andExitCodes <$> traverse runBuild builders
+        if printOrExec
+        then do
+          let btexts :: NonEmpty LT.Text = NE.map renderAsXml builders
+          LT.putStrLn $ LT.unlines $ NE.toList btexts
+          return ExitSuccess
+        else andExitCodes <$> traverse runBuild builders
 
 andExitCode :: ExitCode -> ExitCode -> ExitCode
 andExitCode (ExitFailure i) (ExitFailure j) = ExitFailure (max i j)
@@ -40,10 +47,11 @@ andExitCodes
 andExitCodes = foldr1 andExitCode
 
 {- HLINT ignore Options -}
-data Options = Options { optFilenames :: NonEmpty String }
+data Options = Options { optFilenames :: NonEmpty String, optPrint :: Bool }
 
 optionsParser :: Opt.Parser Options
 optionsParser = Options <$> NE.some1 (Opt.strArgument (Opt.metavar "FILE"))
+                        <*> Opt.switch (Opt.long "print" <> Opt.short 'p' <> Opt.help "print the builder's interpretation, but do not execute it")
 
 optionsParserInfo :: Opt.ParserInfo Options
 optionsParserInfo = Opt.info (optionsParser <**> Opt.helper) Opt.fullDesc
@@ -51,7 +59,8 @@ optionsParserInfo = Opt.info (optionsParser <**> Opt.helper) Opt.fullDesc
 main :: IO ()
 main = do
   options <- Opt.execParser optionsParserInfo
-  codes <- traverse process (optFilenames options)
+  let print :: Bool = optPrint options
+  codes <- traverse (process print) (optFilenames options)  
   exitWith $ andExitCodes codes
 
 -- kept for future reference:
