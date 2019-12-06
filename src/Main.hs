3f64249459609a32b77e8a69e8f7b9e8e2dba9ec
smelc <clement.hurlin@gmail.com>
2019-12-06 15:09:29 +0100

    Haskellize the main, thanks to @polux's suggestion

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
@@ -58,31 +59,6 @@ optionsParserInfo = Opt.info (optionsParser <**> Opt.helper) Opt.fullDesc
 
 main :: IO ()
 main = do
-  options <- Opt.execParser optionsParserInfo
-  let print :: Bool = optPrint options
-  codes <- traverse (process print) (optFilenames options)  
+  Options{optFilenames, optPrint} <- Opt.execParser optionsParserInfo
+  codes <- traverse (process optPrint) optFilenames
   exitWith $ andExitCodes codes
-
--- kept for future reference:
-runSubstedBuild :: Validation (Set.Set ValidationError) Builder -> IO ExitCode
-runSubstedBuild eBuilder =
-  case eBuilder of
-     Failure _       -> return $ ExitFailure 1
-     Success builder -> runBuild builder
-
-renderErrors :: Set.Set ValidationError -> LT.Text
-renderErrors = LT.pack . unlines . map show . Set.toList
-
-main0 :: IO ()
-main0 = do
-  LT.putStrLn (renderAsXml shellCmd0)
-  LT.putStrLn (renderAsXml builder)
-  LT.putStrLn (validation renderErrors renderAsXml substedBuilder)
-  rc :: ExitCode <- runSubstedBuild substedBuilder
-  putStrLn $ "Builder finished with " ++ show rc
-  where shellCmd0 :: Step = ShellCmd ["ls", "$[mydir]"]
-        shellCmd1 = ShellCmd ["tree", "-L", "1", "$[mydir]"]
-        shellCmd2 = ShellCmd ["wrongcmd"]
-        builder :: Builder = Builder "bname" [shellCmd0, shellCmd1, shellCmd2]
-        subst :: Subst = Map.fromList [("mydir", "/")]
-        substedBuilder = substitute ("$[", "]") subst builder
