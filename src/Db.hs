8db5f2d1415b29f31a9b0ecc824f18dd5929e36e
Paul Brauner <polux@google.com>
2020-01-15 09:51:31 +0100

    pass WAL and foreign key pragmas, fix syntax error in table declaration

--- src/Db.hs
+++ src/Db.hs
@@ -33,18 +33,19 @@ tableNameStep = "step"
 
 tableBuilderName :: String = "name"
 
--- TODO smelc Use "PRAGMA journal_mode=WAL;", see lounge's discussion with @polux on January14th; once we have parallel builds
 instance DbOperations IO where
     ensureDB = do
-        connexion <- open dbFile -- TODO: pass PRAGMA foreign_keys = ON
+        connexion <- open dbFile
+        execute_ connexion "PRAGMA journal_mode=WAL;"
+        execute_ connexion "PRAGMA foreign_keys=ON;"
         execute_ connexion createBuilderTable
         execute_ connexion createBuildTable
         execute_ connexion createStepsTable
         close connexion
       where
         createBuilderTable :: Query = "CREATE TABLE IF NOT EXISTS builder (id INTEGER PRIMARY KEY, TEXT name)"
-        createBuildTable :: Query = "CREATE TABLE IF NOT EXISTS build (id INTEGER PRIMARY KEY, builder_id INTEGER, TEXT start, TEXT end, TEXT status)" -- , FOREIGN KEY(builder_id) REFERENCES builder(id) <- requires custom connexion
-        createStepsTable :: Query = "CREATE TABLE IF NOT EXISTS step (id INTEGER PRIMARY KEY, build_id INTEGER, TEXT description, TEXT stdout, TEXT stderr, TEXT status)" -- , FOREIGN KEY(build_id) REFERENCES build(id) <- requires custom connexion
+        createBuildTable :: Query = "CREATE TABLE IF NOT EXISTS build (id INTEGER PRIMARY KEY, builder_id INTEGER, start TEXT, end TEXT, status TEXT, FOREIGN KEY(builder_id) REFERENCES builder(id))"
+        createStepsTable :: Query = "CREATE TABLE IF NOT EXISTS step (id INTEGER PRIMARY KEY, build_id INTEGER, description TEXT, stdout TEXT, stderr TEXT, status TEX, FOREIGN KEY(build_id) REFERENCES build(id))"
     getBuilderID = undefined
     startBuild = undefined
     recordStep = undefined
