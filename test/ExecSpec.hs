bc5c082cadfcc202443433984df22589e9bbf83d
Paul Brauner <polux@google.com>
2020-01-24 12:26:29 +0100

    tests Exec with fake DB

--- test/ExecSpec.hs
+++ test/ExecSpec.hs
@@ -3,13 +3,19 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module ExecSpec (spec) where
 
-import Common (Status(Success))
+import Common hiding (Error)
 import Config
+import Control.Lens
 import Control.Monad.Freer
 import Control.Monad.Freer.Error
+import Control.Monad.Freer.State
 import Control.Monad.Freer.Writer
 import Data.Function
 import Db
@@ -17,6 +23,9 @@ import Data.Text.Prettyprint.Doc.Render.Terminal
 import Exec
 import System.Exit
 import Test.Hspec
+
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 -- Mock behavior for shell commands, used by both LoggingMockExec and
 -- TracingMockExec
@@ -62,6 +71,16 @@ runExecAsExecutionLog = runWriter . execToWriter
     PutOut str -> return ()
     PutErr str -> return ()
 
+runStubExec
+  :: Eff (Exec ': effs) a
+  -> Eff effs a
+runStubExec = interpret $ \case
+  ZzLog color entry -> return ()
+  RunShellCommand workdir command ->
+    return (mockShellCommand command)
+  PutOut str -> return ()
+  PutErr str -> return ()
+
 runStubDbOps
   :: Eff (DbOperations ': effs)
   ~> Eff effs
@@ -70,6 +89,74 @@ runStubDbOps = interpret $ \case
    StartStep state desc -> return 0
    EndStep state stepID streams status -> return state
    EndBuild state -> return Success
+
+-- FakeDb mock exec
+
+newtype BuildEntry = BuildEntry
+  { buildEntryName :: String
+  }
+  deriving (Eq, Ord, Show)
+
+data StepEntry = StepEntry
+  { stepEntryStep :: Step Substituted
+  , stepEntryStreams :: Maybe StepStreams
+  , stepEntryStatus :: Maybe Status
+  }
+  deriving (Eq, Ord, Show)
+
+data FakeDb = FakeDb
+  { _fakeDbCounter :: Int
+  , _fakeDbBuilds :: M.Map BuildID BuildEntry
+  , _fakeDbSteps :: M.Map (StepID, BuildID) StepEntry
+  }
+  deriving (Eq, Ord, Show)
+
+makeLenses ''FakeDb
+
+emptyFakeDb = FakeDb 0 mempty mempty
+
+type NormalizedFakeDb = (M.Map BuildEntry (S.Set StepEntry))
+
+normalizeFakeDb :: FakeDb -> NormalizedFakeDb
+normalizeFakeDb (FakeDb _ builds steps) =
+  M.fromList [(build, stepsFor id) | (id, build) <- M.toList builds]
+ where
+  stepsFor parentId = S.fromList
+    [step | ((_, buildId), step) <- M.toList steps, buildId == parentId]
+
+freshId
+  :: Member (State FakeDb) effs
+  => Eff effs Int
+freshId = do
+  modify (fakeDbCounter +~ 1)
+  gets _fakeDbCounter
+
+runDbOpsWithFakeDb
+  :: FakeDb
+  -> Eff (DbOperations ': effs) a
+  -> Eff effs (a, FakeDb)
+runDbOpsWithFakeDb db = runState db . dbToState
+
+dbToState
+  :: Eff (DbOperations ': effs)
+  ~> Eff (State FakeDb ': effs)
+dbToState = reinterpret $ \case
+  (StartBuild name) -> do
+    buildId <- freshId
+    modify $ fakeDbBuilds . at buildId ?~ BuildEntry name
+    return (BuildState buildId Success)
+  (EndBuild (BuildState buildId status)) ->
+    return status
+  (StartStep (BuildState buildId _) step) -> do
+    stepId <- freshId
+    modify $ fakeDbSteps . at (stepId, buildId) ?~ StepEntry step Nothing Nothing
+    return stepId
+  (EndStep (BuildState buildId buildStatus) stepId streams status) -> do
+    modify $ fakeDbSteps . ix (stepId, buildId) %~ updateStepEntry streams status
+    return (BuildState buildId (max buildStatus status))
+ where
+  updateStepEntry streams status (StepEntry step _ _) =
+    StepEntry step (Just streams) (Just status)
 
 -- Tests
 
@@ -81,6 +168,9 @@ spec =
     it "should set the working directory as specified" $
       (process Execute env testXml & runError & runExecAsExecutionLog & runStubDbOps & run)
         `shouldBe` expectedTrace
+    it "should write the expected information in the DB" $
+      (process Execute env testXml & runError @ExitCode & runStubExec & runDbOpsWithFakeDb emptyFakeDb & run & snd & normalizeFakeDb)
+        `shouldBe` expectedDb
   where
     env = ProcessEnv "/test/workdir" [("ENV_VAR", "a")]
     testXml =
@@ -119,3 +209,64 @@ spec =
         , Execution "/test/workdir/dir1" (Command "some junk 2")
         ]
       )
+    expectedDb =
+      M.fromList
+        [ ( BuildEntry {buildEntryName = "test"}
+          , S.fromList
+              [ StepEntry
+                  { stepEntryStep = ShellCmd
+                      { workdir = "/test/workdir/dir1"
+                      , cmd = Command "ls" ["a"]
+                      , mprop = Nothing
+                      , haltOnFailure = True
+                      }
+                  , stepEntryStreams = Just (StepStreams (Just "foo bar") (Just ""))
+                  , stepEntryStatus = Just Success
+                  }
+              , StepEntry
+                  { stepEntryStep = ShellCmd
+                      { workdir = "/test/workdir/dir1/dir2"
+                      , cmd = Command "ls" ["b"]
+                      , mprop = Nothing
+                      , haltOnFailure = True
+                      }
+                  , stepEntryStreams =
+                      Just (StepStreams (Just "bar baz") (Just ""))
+                  , stepEntryStatus = Just Success
+                  }
+              , StepEntry
+                  { stepEntryStep = ShellCmd
+                      { workdir = "/absolute/dir"
+                      , cmd = Command "ls" ["b"]
+                      , mprop = Nothing
+                      , haltOnFailure = True
+                      }
+                  , stepEntryStreams =
+                      Just (StepStreams (Just "bar baz") (Just ""))
+                  , stepEntryStatus = Just Success
+                  }
+              , StepEntry
+                  { stepEntryStep = ShellCmd
+                      { workdir = "/test/workdir/dir1"
+                      , cmd = Command "some" ["junk", "1"]
+                      , mprop = Nothing
+                      , haltOnFailure = False
+                      }
+                  , stepEntryStreams =
+                      Just (StepStreams (Just "") (Just "command not found"))
+                  , stepEntryStatus = Just Failure
+                  }
+              , StepEntry
+                  { stepEntryStep = ShellCmd
+                      { workdir = "/test/workdir/dir1"
+                      , cmd = Command "some" ["junk", "2"]
+                      , mprop = Nothing
+                      , haltOnFailure = True
+                      }
+                  , stepEntryStreams =
+                      Just (StepStreams (Just "") (Just "command not found"))
+                  , stepEntryStatus = Just Failure
+                  }
+              ]
+          )
+        ]
